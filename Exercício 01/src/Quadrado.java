e9e83c6e5bff0e6b5de815464dab5b27b59bbf4a
Giovani <giovanigomes2002@gmail.com>
2023-01-20 22:18:20 -0300

    Exercício 01
    Resolução do Exercícios 01.

---
+++ Exercício 01/src/Quadrado.java
@@ -0,0 +1,11 @@
+public class Quadrado extends Figura2D{
+    private int lados;
+
+    public int getLados() {
+        return lados;
+    }
+
+    public void setLados(int lados) {
+        this.lados = lados;
+    }
+}
---
+++ Exercício 01/src/Triangulo.java
@@ -0,0 +1,23 @@
+public class Triangulo extends Figura2D  {
+
+    private double base;
+
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    private double altura
+            ;
+}
